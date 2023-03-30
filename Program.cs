7639feeec952dd932d57f21021170b1aa71aecde
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-03-30 11:00:46 -0400

    difficulty select: not an option and replay

--- Program.cs
+++ Program.cs
@@ -7,14 +7,18 @@
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.BackgroundColor = ConsoleColor.DarkGray;
             Console.Clear();
+            Random rand = new Random();
             string word = "";
-            string displayWord = "_ _ _ _ _ _ _ _";
+            string displayWord = "";
             string guess;
+            string replay;
             int wrongGuesses = 0;
             int totalGuesses = 0;
             int difficulty = 0;
             bool done = false;
+            bool done1 = false;
             List<string> words = new List<string>();
+            List<string> wrongLetters = new List<string>();
 
             Console.WriteLine();
             Console.WriteLine("  HANGMAN LITE");
@@ -33,20 +37,20 @@
             Wait20();
             Console.WriteLine(" After enough wrong guesses, the man will die and you LOSE. ");
             Wait20();
-            
+
             Console.WriteLine();
             Console.Write("Press ENTER to begin ");
             Console.ReadLine();
             Console.WriteLine();
 
-            DrawHanger();
-            Console.WriteLine();
-            Console.WriteLine(displayWord);
-            Console.WriteLine();
 
-
-            while (!done)
+            do
             {
+                DrawHanger();
+                Console.WriteLine();
+                Console.WriteLine();
+
+
                 Console.WriteLine("What difficulty would you like? ");
                 Console.WriteLine("1 - Easy ");
                 Console.WriteLine("2 - Medium ");
@@ -60,90 +64,134 @@
 
                 switch (difficulty)
                 {
-                    case 1: Console.WriteLine("Difficulty set to: Easy ");
+                    case 1:
+                        Console.WriteLine("Difficulty set to: Easy ");
                         foreach (var s in easyWords)
                             words.Add(s); break;
-                    case 2: Console.WriteLine("Difficulty set to: Medium ");
+                    case 2:
+                        Console.WriteLine("Difficulty set to: Medium ");
                         foreach (var s in mediumWords)
                             words.Add(s); break;
-                    case 3: Console.WriteLine("Difficulty set to: Hard ");
-                        foreach (var s in hardWords) 
+                    case 3:
+                        Console.WriteLine("Difficulty set to: Hard ");
+                        foreach (var s in hardWords)
                             words.Add(s); break;
+                    default: 
+                        Console.WriteLine("That is not an option! ")
                 }
-                Console.WriteLine(words[0] + words[1]);
 
+                word = words[rand.Next(0, words.Count)];
 
-                Console.Write("Pick a letter:  ");
-                guess = Console.ReadLine().ToUpper();
-
-
-                if (guess.Length == 1)
+                for (int i = 0; i < word.Length; i++)
                 {
-                    if (word.Contains(guess))
+                    if (i % 2 != 0)
+                        displayWord = displayWord.Insert(i, " ");
+                    else
+                        displayWord = displayWord.Insert(i, "_");
+                }
+
+                Wait5();
+                Console.WriteLine();
+                Console.WriteLine(displayWord);
+                Console.WriteLine();
+
+                while (!done)
+                {
+                    Console.Write("Pick a letter:  ");
+                    guess = Console.ReadLine().ToUpper();
+
+
+                    if (guess.Length == 1)
                     {
-                        Wait5();
+                        if (word.Contains(guess))
+                        {
+                            Wait5();
+                            Console.WriteLine();
+                            Console.WriteLine("That letter IS in the word! ");
+                            Console.WriteLine();
+                            Wait5();
+                            displayWord = displayWord.Remove(word.IndexOf(guess), 1);
+                            displayWord = displayWord.Insert(word.IndexOf(guess), guess);
+                        }
+                        else
+                        {
+                            Wait5();
+                            Console.WriteLine();
+                            Console.WriteLine("That letter is NOT in the word! ");
+                            Console.WriteLine();
+                            Wait5();
+                            wrongGuesses++;
+                            wrongLetters.Add(guess);
+
+                            foreach (string j in wrongLetters)
+                            {
+                                Console.Write($"{j}, ");
+                            }
+                            Console.WriteLine();
+                        }
+
+                        totalGuesses++;
+
+                        switch (wrongGuesses)
+                        {
+                            case 1: DrawHead(); break;
+                            case 2: DrawArmL(); break;
+                            case 3: DrawArmR(); break;
+                            case 4: DrawLegL(); break;
+                            case 5: DrawLegR(); break;
+                        }
+
+                        Console.WriteLine(displayWord);
                         Console.WriteLine();
-                        Console.WriteLine("That letter IS in the word! ");
-                        Console.WriteLine();
-                        Wait5();
-                        displayWord = displayWord.Remove(word.IndexOf(guess), 1);
-                        displayWord = displayWord.Insert(word.IndexOf(guess), guess);
+                        Wait10();
                     }
                     else
                     {
                         Wait5();
                         Console.WriteLine();
-                        Console.WriteLine("That letter is NOT in the word! ");
+                        Console.WriteLine("You can only guess one letter at a time! ");
                         Console.WriteLine();
+
                         Wait5();
-                        wrongGuesses++;
                     }
 
-                    totalGuesses++;
-
-                    switch (wrongGuesses) 
+                    if (displayWord == word)
                     {
-                        case 1: DrawHead(); break;
-                        case 2: DrawArmL(); break;
-                        case 3: DrawArmR(); break;
-                        case 4: DrawLegL(); break;
-                        case 5: DrawLegR(); break;
+                        DrawWin();
+                        Console.WriteLine("You guessed the word correctly and saved the man! ");
+                        Console.WriteLine();
+                        Wait20();
+                        done = true;
                     }
 
-                    Console.WriteLine(displayWord);
-                    Console.WriteLine();
-                    Wait10();
-                }
-                else
-                {
-                    Wait5();
-                    Console.WriteLine();
-                    Console.WriteLine("You can only guess one letter at a time! ");
-                    Console.WriteLine();
-                    Wait5();
+                    if (wrongGuesses == 5)
+                    {
+                        Console.WriteLine("You died! ");
+                        Console.WriteLine($"The word was {word}! ");
+                        Console.WriteLine();
+                        Wait20();
+                        done = true;
+                    }
                 }
 
-                if (displayWord == word)
-                {
-                    DrawWin();
-                    Console.WriteLine("You guessed the word correctly and saved the man! ");
-                    Console.WriteLine();
-                    Wait20();
-                    done = true;
-                }
+                Console.WriteLine($"Your wrong guesses: {wrongGuesses} ");
+                Console.WriteLine($"Your total guesses: {totalGuesses} ");
 
-                if (wrongGuesses == 5)
-                {
-                    Console.WriteLine("You died! ");
-                    Console.WriteLine($"The word was {word}! ");
-                    Console.WriteLine();
-                    Wait20();
-                    done = true;
-                }
-            }
 
-            Console.WriteLine($"Your wrong guesses: {wrongGuesses} ");
-            Console.WriteLine($"Your total guesses: {totalGuesses} ");
+                Console.WriteLine("Would you like to play again? ");
+                Console.Write("Choice:  ");
+                replay = Console.ReadLine().ToUpper();
+
+                if (replay == "YES" || replay == "Y")
+                {
+                    Console.WriteLine("Continuing... ");
+                }
+                else if (replay == "Q" || replay == "N" || replay == "NO")
+                {
+                    Console.WriteLine("Quitting... ");
+                    done1 = true;
+                }
+            } while (!done1);
         }
 
 
